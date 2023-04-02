1dc367d275de498b0510a1a358db1dbdec8623a0
Alejandro Fernandez <fernandezale007@gmail.com>
2023-04-02 16:03:28 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,50 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+
+describe('introduction(name)', function () {
+    it('takes in an argument of a name and returns a phrase with that name using string interpolation', function () {
+        expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
+        expect(introduction("Samip")).toEqual("Hi, my name is Samip.");
+    });
+});
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+
+describe('introductionWithLanguage(name, language)', function () {
+    it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function () {
+        expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
+        expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
+    })
+})
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
+describe('introductionWithLanguageOptional(name, language)', function () {
+    it('takes in two arguments, a name and a language, and language defaults to JavaScript', function () {
+        expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
+    })
+})
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+describe('introductionWithLanguageOptional(name, language)', function () {
+    it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function () {
+        expect(introductionWithLanguageOptional("Gracie", "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
+    })
+})
+
+
